125b20d663846ae4231fe8ae0f6fd66d8673da66
Fredrik Hagfjäll <fredrik@hagfjall.se>
2015-11-01 11:36:00 +0100

    Loading last id that didnt returned 404

--- src/main/java/Tst.java
+++ src/main/java/Tst.java
@@ -1,10 +1,12 @@
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -17,7 +19,15 @@ public class Tst {
 
     public static void main(String[] args) throws IOException {
         File workingUrls = new File("workingsUrls.txt");
-        for (int i = 447; i < 21684046; i++) {
+        int currentId;
+        try {
+            String lastUrl = new ReversedLinesFileReader(workingUrls).readLine();
+            currentId = Integer.parseInt(lastUrl.replaceAll("[^-?0-9]+", ""));
+        } catch (FileNotFoundException exception) {
+            currentId = 446;
+        }
+
+        for (int i = currentId+1; i < 21684046; i++) {
             String url = baseUrl + i + ".ab";
             Connection conn = Jsoup.connect(url);
             conn.method(Connection.Method.HEAD);
@@ -27,7 +37,6 @@ public class Tst {
                 final int statusCode = conn.response().statusCode();
                 System.out.println(url);
                 FileUtils.writeStringToFile(workingUrls, url + "\n", true);
-                
             } catch (HttpStatusException e) {
                 System.err.println(e.getStatusCode() + " on " + url);
             }
