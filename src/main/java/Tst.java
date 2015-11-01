31a0d8a955dc311fcaf2eca2f2f99d10df2969ea
Fredrik Hagfjäll <fredrik@hagfjall.se>
2015-11-01 11:21:33 +0100

    Now appends the working urls to a text file

--- src/main/java/Tst.java
+++ src/main/java/Tst.java
@@ -1,13 +1,12 @@
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.*;
 /**
  * Created by kerling on 01/11/15.
  */
@@ -15,10 +14,9 @@ public class Tst {
 
     private static String baseUrl = "http://www.aftonbladet.se/article";
 
-    private static List<String> workingUrls = new ArrayList<String>();
 
     public static void main(String[] args) throws IOException {
-        IOUtils.toString();
+        File workingUrls = new File("workingsUrls.txt");
         for (int i = 447; i < 21684046; i++) {
             String url = baseUrl + i + ".ab";
             Connection conn = Jsoup.connect(url);
@@ -28,7 +26,8 @@ public class Tst {
                 Document document = conn.get();
                 final int statusCode = conn.response().statusCode();
                 System.out.println(url);
-                workingUrls.add(url);
+                FileUtils.writeStringToFile(workingUrls, url + "\n", true);
+                
             } catch (HttpStatusException e) {
                 System.err.println(e.getStatusCode() + " on " + url);
             }
