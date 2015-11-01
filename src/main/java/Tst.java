34aad04c2fa85d99018220b68cf6b38037b7df15
Fredrik Hagfjäll <fredrik@hagfjall.se>
2015-11-01 15:21:00 +0100

    Using the java-version of HTTP connetion, much faster\!

--- src/main/java/Tst.java
+++ src/main/java/Tst.java
@@ -1,13 +1,11 @@
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.input.ReversedLinesFileReader;
-import org.jsoup.Connection;
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by kerling on 01/11/15.
@@ -27,18 +25,17 @@ public class Tst {
             currentId = 446;
         }
 
-        for (int i = currentId+1; i < 21684046; i++) {
-            String url = baseUrl + i + ".ab";
-            Connection conn = Jsoup.connect(url);
-            conn.method(Connection.Method.HEAD);
-            conn.followRedirects(true);
-            try {
-                Document document = conn.get();
-                final int statusCode = conn.response().statusCode();
+        for (int i = currentId + 1; i < 21684046; i++) {
+            URL url = new URL(baseUrl + i + ".ab");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setInstanceFollowRedirects(false);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 301 || responseCode == 200) {
                 System.out.println(url);
                 FileUtils.writeStringToFile(workingUrls, url + "\n", true);
-            } catch (HttpStatusException e) {
-                System.err.println(e.getStatusCode() + " on " + url);
+            }else {
+                System.out.println(responseCode + " on " + url);
             }
         }
     }
