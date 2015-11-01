5306e6bba54ffae4aeaaef4f764f7c15360e865e
Tommy Welleby <tommywelleby@gmail.com>
2015-11-01 20:12:02 +0100

    Fixed move of Article

--- src/main/java/org/welleby/web/scrape/aftonbladet/Article.java
+++ src/main/java/org/welleby/web/scrape/aftonbladet/Article.java
@@ -1,7 +1,6 @@
 package org.welleby.web.scrape.aftonbladet;
 
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import java.sql.Date;
 
@@ -10,8 +9,10 @@ import java.sql.Date;
  */
 public class Article {
 
-    private static final String ARTICLE_DIV = "abMd6 abLefty abArticle ";
+    private static final String ARTICLE_DIV = "abBodyText clearfix abJsBodyText";
+    private static final String AUTHOR_DIV = "abIconMail abAuthorMail abPfxPrimary";
     private String title, body, author;
+    private int wordCount;
 
     private Date timestamp;
 
@@ -36,9 +37,15 @@ public class Article {
     }
 
     private void parseDocument(Document doc) {
-        Elements elementsByClass = doc.body().getElementsByAttributeValueContaining("class",ARTICLE_DIV);
-        elementsByClass.html();
-        System.out.println(elementsByClass);
+        body = doc.body().getElementsByAttributeValueContaining("class", ARTICLE_DIV).text();
+//        This doesn' work since it's a span class and not a div class, look at http://stackoverflow.com/questions/9728854/jsoup-to-get-text-from-span-class for a solution
+//        author = doc.body().getElementsByAttributeValueContaining("class", AUTHOR_DIV).text();
+        title = doc.title().substring(0,doc.title().indexOf('|')).trim();
+        wordCount = body.trim().split("\\s+").length;
+        System.out.println(title);
+        System.out.println(body);
+//        System.out.println(author);
+        System.out.println(wordCount);
 
     }
 }
